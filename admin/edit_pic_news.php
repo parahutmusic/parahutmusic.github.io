724828b0e962e2ea0e094eb0227689d4f6bb675f
echangzt <chotipat88@hotmail.com>
2017-04-08 23:26:21 +0700

    edit_css

--- admin/edit_pic_news.php
+++ admin/edit_pic_news.php
@@ -5,12 +5,33 @@ include "../admin/dblink.php";
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
+<meta charset="UTF-8">
+    <title>admin parahut</title>
+    <link href="../admin/css/menu.css" rel="stylesheet">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- icon fonts font Awesome -->
+	<link href="../assets/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Import Magnific Pop Up Styles -->
+	<link rel="stylesheet" href="../assets/css/magnific-popup.css">
+
+	<!-- Import Custom Styles -->
+	<link href="../assets/css/style.css" rel="stylesheet">
+
+	<!-- Import Animate Styles -->
+	<link href="../assets/css/animate.min.css" rel="stylesheet">
+
+	<!-- Import owl.carousel Styles -->
+	<link href="../assets/css/owl.carousel.css" rel="stylesheet">
+
+	<!-- Import Custom Responsive Styles -->
+	<link href="../assets/css/responsive.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
 </head>
 
 <body>
-<?php include "../admin/admin_parahut.php"; ?>
+
 <div class="container top">
 <form id="form-img1" method="post" action="edit_pic_news_save.php" enctype="multipart/form-data"> 
     <?php 
