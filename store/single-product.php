0039e6261f5b1b3071847f590b9179e377344983
changchaly14 <you@example.com>
2017-04-16 23:41:13 +0700

    test

--- store/single-product.php
+++ store/single-product.php
@@ -1,15 +1,15 @@
-<?php require_once("dblink.php"); 
+<?php include "dblink.php"; 
 
 $sql5 = "SELECT * FROM products where pro_id = '$pro_id'";
-$link_query_5 = mysqli_query($link, $sql5);
-$rsm5 = mysqli_fetch_array($link_query_5);
+$link_query = mysqli_query($link, $sql5);
+$rsm5 = mysqli_fetch_array($link_query);
 
 $pro_id = $rsm5['pro_id'];
 $pro_view = $rsm5['pro_view'];
 $count = $pro_view + 1;
  
-$sql= "update products set pro_view = $count WHERE pro_id = $pro_id";
-    mysqli_query($link ,$sql);
+$sql6 = "update products set pro_view = $count WHERE pro_id = $pro_id";
+$query  = mysqli_query($link ,$sql6);
 ?>
 <!DOCTYPE html>
 <html lang="en">
