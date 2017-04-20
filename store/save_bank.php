be965ac3d540c105b2f36210d4b562da40548c64
changchaly14 <you@example.com>
2017-04-20 14:27:23 +0700

    add_check_bank

--- store/save_bank.php
+++ store/save_bank.php
@@ -3,7 +3,7 @@
 <?php 	
 		$pay_id = $_POST['pay_id'];
 		$pro_id 	= $_POST['pro_id'];
-		$order_id 	= $_POST['order_id'];
+		$order_id2 	= $_POST['order_id'];
 		$name 	= $_POST['name'];
 		$phone 	= $_POST['phone'];
 		$money 	= $_POST['money'];
@@ -16,6 +16,14 @@
   	copy($_FILES['img_bank']['tmp_name'],$pro_pic_file1); 
  
    $pic_file1 = substr($pro_pic_file1,4);
+
+   	  $sql = "SELECT * FROM tb_order WHERE order_id = '$order_id2' ";
+	  $db_query = mysqli_query($link, $sql);
+	  $num_rows  = mysqli_num_rows($db_query);
+	  $rs = mysqli_fetch_array($db_query);  
+
+    	$order_id = $rs['order_id'];
+
    
 		if(empty($bank))
 		{
@@ -23,16 +31,23 @@
 			echo "<script langquage='javascript'>\n";
 			echo " window.location=\"check-order-detail.php\"\n";
 			echo	"</script>\n";	
-		}else{	
-		if($_FILES['img_bank']['name']=='')
+		}else{
+			if($order_id2 != "$order_id")
 		{
-			echo "<script>alert('กรุณาเพิ่มรูปหลักฐาน');history.back();</script>";
+			echo "<script>alert('ไม่มีเลขที่ใบสั่งซื้อ');history.back();</script>";
 			echo "<script langquage='javascript'>\n";
 			echo " window.location=\"check-order-detail.php\"\n";
 			echo	"</script>\n";	
 		}else{
+			if($_FILES['img_bank']['name']=='')
+			{
+				echo "<script>alert('กรุณาเพิ่มรูปหลักฐาน');history.back();</script>";
+				echo "<script langquage='javascript'>\n";
+				echo " window.location=\"check-order-detail.php\"\n";
+				echo	"</script>\n";	
+			}else{
 	$sql="insert into payments values('$pay_id', 
-		'$order_id', 
+		'$order_id2', 
 		'$name',
 		'$bank',
 		'$pro_pic_file1',
@@ -46,6 +61,7 @@
 					echo	"</script>\n";
 			}
 		}
+	}
 ?>
 
 
