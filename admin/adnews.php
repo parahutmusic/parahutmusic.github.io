d309db7283908b642133f0d093bc02f22a064fc6
changchaly14 <you@example.com>
2017-04-24 00:23:09 +0700

    ad_news

--- admin/adnews.php
+++ admin/adnews.php
@@ -29,11 +29,6 @@ include "../admin/dblink.php";
 	<link href="../assets/css/responsive.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
 </head>
 <style>
 @import url('https://fonts.googleapis.com/css?family=Kanit');
